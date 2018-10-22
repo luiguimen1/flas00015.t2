de5822e93ae2fadafeb43230e5ad0acb61d3f9d8
Luis Eduardo Alvarez Meneses <luiguimen1@hotmail.com>
2018-10-22 12:03:22 -0500

    Version 01

--- src/pages/operaciones/operaciones.ts
+++ src/pages/operaciones/operaciones.ts
@@ -18,7 +18,7 @@ export class OperacionesPage {
     numero2;
     error1: boolean;
     error2: boolean;
-    resultado:number;
+    resultado;
     constructor(public navCtrl: NavController, public navParams: NavParams) {
         this.error1 = false;
         this.error2 = false;
@@ -30,19 +30,37 @@ export class OperacionesPage {
             setTimeout(() => {
                 this.error1 = false;
             }, 2000);
-        }else if (isNaN(parseFloat(this.numero2))) {
+        } else if (isNaN(parseFloat(this.numero2))) {
             this.error2 = true;
             setTimeout(() => {
                 this.error2 = false;
             }, 2000);
-        }else{
-            this.resultado = parseFloat(this.numero1) + parseFloat(this.numero2);   
-        }       
+        } else {
+            this.resultado = parseFloat(this.numero1) + parseFloat(this.numero2);
+        }
     }
 
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad OperacionesPage');
+    }
+
+    edificio;
+
+    tabla() {
+        this.edificio = Array();
+        let cadena="listado de resultados= <br>";
+        for (let i = 0; i < this.numero2; i++) {
+            let piso: any={
+                numero: this.numero1,
+                por: (i + 1),
+                resul: (this.numero1 * (i + 1))
+            }
+            this.edificio.push(piso);
+            cadena += piso.numero + " * "+ piso.por + " = "+ piso.resul+"<br>";
+        }
+        
+        this.resultado = cadena;
     }
 
 }
